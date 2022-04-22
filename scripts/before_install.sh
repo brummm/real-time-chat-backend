3b5db5183a2ff5d29434867da1192bccd6a90e8e
Rogerio Brum <rogeriobrum@gmail.com>
2022-04-22 20:29:48 -0300

    Change the way node is installed into the aws instance

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,9 @@ if which node > /dev/null then
     echo "node is installed, skipping..."
 else
   # using a snap package to install node at version 16
-  sudo snap install node --classic --channel=16
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  . ~/.nvm/nvm.sh
+  nvm install node
 fi
 
 DIR="/home/ec2-user/real-time-chat"
