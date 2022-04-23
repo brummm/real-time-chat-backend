1a92d9729114992ea64bfcde00f2742e0d7a675f
Rogerio Brum <rogeriobrum@gmail.com>
2022-04-23 16:32:12 -0300

    Commit to test deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # install node
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
-yum -y install nodejs
+sudo yum install -y gcc-c++ make
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 18
+nvm alias default 18.0.0
 
 # install mongo
-tee -a /etc/yum.repos.d/mongodb-org-5.0.repo <<EOF
+sudo tee -a /etc/yum.repos.d/mongodb-org-5.0.repo <<EOF
 [mongodb-org-5.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/5.0/x86_64/
@@ -15,6 +18,8 @@ gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
 EOF
 
 sudo yum install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
 
 
 DIR="/home/ec2-user/real-time-chat"
