9742033cc7604a44078b1cc93e4a15fa89bf2dfc
Rogerio Brum <rogeriobrum@gmail.com>
2022-04-22 21:02:05 -0300

    Changes before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# install node and npm if they don't exist
-if [ which node > /dev/null ]
-then
-    echo "node is installed, skipping...";
-else
-  # using a snap package to install node at version 16
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-  . ~/.nvm/nvm.sh
-  nvm install node
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
 DIR="/home/ec2-user/real-time-chat"
 if [ -d "$DIR" ]; then
